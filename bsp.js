88a57656dd73f0c95ca07097e6606d534000ee95
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-12 20:58:00 +0100

    Add files via upload

--- bsp.js
+++ bsp.js
@@ -52,7 +52,7 @@ var A09Server;
         if (_request.url) {
             let url = Url.parse(_request.url, true);
            
-                let jsonString = JSON.stringify(result.query);
+                let jsonString = JSON.stringify(result);
                 _response.write(jsonString);
                 console.log(jsonString);
             
