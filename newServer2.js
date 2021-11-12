29bf62ea9bda8f8bd20cd1bfe976312d2dba7f57
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-12 17:44:40 +0100

    Add files via upload

--- newServer2.js
+++ newServer2.js
@@ -53,7 +53,7 @@ var Endabgabe;
         
           console.log(result);
 
-          const server = Http.createServer(function (res) {
+          const server = Http.Server(function (res) {
           res.writeHead(302, {
             location: "https://google.com"});
           
