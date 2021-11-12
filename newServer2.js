4a63c475c5c4837ffeddd4c3284af0bf12733b48
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-12 19:41:12 +0100

    Add files via upload

--- newServer2.js
+++ newServer2.js
@@ -1,35 +1,43 @@
 "use strict";
-let button1 = document.createElement("button");
-button1.innerText = "html";
-button1.setAttribute("type", "button");
-button1.addEventListener("click", submitVerarbeiten);
-document.getElementById("button")?.appendChild(button1);
-let button2 = document.createElement("button");
-button2.innerText = "json";
-button2.setAttribute("type", "button");
-button2.addEventListener("click", submitVerarbeiten2);
-document.getElementById("button2")?.appendChild(button2);
-async function submitVerarbeiten() {
-    let formData = new FormData(document.forms[0]);
-    let url = "https://gismaximilianmuenchinger.herokuapp.com/html";
-    let query = new URLSearchParams(formData);
-    url += "?" + query.toString();
-    let response = await fetch(url);
-    let answer = await response.text();
-    let answerSplit = answer.split("&");
-    let answerSplitBreak = answerSplit.join("<br>");
-    document.getElementById("html").innerHTML = answerSplitBreak;
-}
-async function submitVerarbeiten2() {
-    let formData = new FormData(document.forms[0]);
-    let url = "https://gismaximilianmuenchinger.herokuapp.com/json";
-    let query = new URLSearchParams(formData);
-    url += "?" + query.toString();
-    console.log(url);
-    let response2 = await fetch(url);
-    let answer = await response2.text();
-    //  let answerSplit = answer.split("/");
-    // let answerSplitBreak: string = answerSplit.join("<br>");
-    //  let answerSplitJson = JSON.parse(answerSplitBreak);
-    console.log(answer);
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A09Server = void 0;
+const Http = require("http");
+const Url = require("url");
+var A09Server;
+(function (A09Server) {
+    console.log("Starting server"); // in Konsole wird "starting server" ausgegeben
+    let port = Number(process.env.PORT);
+    // wenn der port keinen Wert hat wird im number 8100 zugewiesen.
+    if (!port)
+        port = 8100;
+    // Instanz "server" wird erstellt, dann wird ein Server erstellt
+    let server = Http.createServer();
+    server.addListener("request", handleRequest);
+    server.addListener("listening", handleListen);
+    server.listen(port);
+    //Server soll port abhören
+    function handleListen() {
+        console.log("Listening"); // in Konsole wird "Listening" ausgegeben wenn der addListener ausgeführt wird
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!"); // in Konsole wird "i hear voices" ausgegeben wenn der addListener ausgeführt wird
+        console.log("test1234");
+        //response parameter
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            if (url.pathname == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ":" + url.query[key] + "<br/>");
+                }
+            }
+            if (url.pathname == "/json") {
+                let jsonString = JSON.stringify(url.query);
+                _response.write(jsonString);
+                console.log(jsonString);
+            }
+        }
+        _response.end();
+    }
+})(A09Server = exports.A09Server || (exports.A09Server = {}));
